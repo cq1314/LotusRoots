062bba3b44c66f49a01bfe6d8222c6158c5a3f41
separal <separal@crimson.ua.edu>
2024-09-23 13:10:24 -0500

    More changes

--- Quiz Front End/Quiz Front End/ClientApp/src/components/NavMenu.js
+++ Quiz Front End/Quiz Front End/ClientApp/src/components/NavMenu.js
@@ -25,7 +25,7 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">Quiz_Front_End</NavbarBrand>
+          <NavbarBrand tag={Link} to="/">Lotus Roots</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
@@ -33,10 +33,10 @@ export class NavMenu extends Component {
                 <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/counter">Quiz</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/fetch-data">Cultural Events</NavLink>
               </NavItem>
             </ul>
           </Collapse>
