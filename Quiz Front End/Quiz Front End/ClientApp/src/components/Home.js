432cbe4ca674b07f0a71e5207e25614bf4e742c8
separal <separal@crimson.ua.edu>
2024-09-24 13:39:10 -0500

    New UI changes

--- Quiz Front End/Quiz Front End/ClientApp/src/components/Home.js
+++ Quiz Front End/Quiz Front End/ClientApp/src/components/Home.js
@@ -1,30 +1,45 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 export class Home extends Component {
     static displayName = Home.name;
 
     render() {
         return (
-            <div style={styles.home}>
-                
-            </div>
+            <>
+                <div className="image-container">
+                    <img src={require('../assets/pinklogo4.png')} alt="Japanese Culture" className="top-image" />
+                </div>
+                <div className="bottomtext">
+                    <h3>Find Your Roots</h3>
+                </div>
+                <div className="image1-container">
+                    <img src={require('../assets/adobe1.jpeg')} alt="Full Width" className="image1" />
+                    <div className="overlay">
+                        <p>Explore Japan's Beauty</p>
+                    </div>
+                </div>
+                <div className="image2-container">
+                    <img src={require('../assets/adobe2.jpeg')} alt="Full Width" className="image2" />
+                    <div className="overlay">
+                        <p>Explore Japan's Cuisine</p>
+                    </div>
+                </div>
+                <div className="image3-container">
+                    <img src={require('../assets/adobe3.jpeg')} alt="Full Width" className="image3" />
+                    <div className="overlay">
+                        <p>Explore Japan's Culture</p>
+                    </div>
+                </div>
+
+                <div className="information">
+                    <p>When it comes to embarking on a captivating journey into the rich and diverse tapestry of Japanese culture, the possibilities can seem overwhelming. However, thanks to a multitude of dedicated organizations across every state, you have a direct gateway to the cultural experiences that resonate with you the most. These organizations are committed to providing invaluable resources, connections, and opportunities, enabling you to fully immerse yourself in the traditions and beauty of Japanese culture.</p>
+                    <br></br>
+                    <br></br>
+                    <p>Here, we are enthusiastic about facilitating your cultural exploration, offering valuable guidance and insights to assist you in discovering the ideal organizations for your interests. With a simple click of ‘learn more,’ you can access comprehensive lists of organizations conveniently grouped by state, making it effortless to connect with the cultural encounters you yearn for. So why wait? Embark on your exploration today and quench your thirst for knowledge and adventure!</p>
+                    <button className="explore-button">Learn More</button>
+                </div>
+            </>
         );
     }
 }
-
-const styles = {
-    home: {
-        fontFamily: 'Georgia',
-        textAlign: 'center',
-        padding: '60px',
-       // Slightly transparent background
-        borderRadius: '10px',
-        maxWidth: '1000px',
-        margin: 'auto',
-        height: '500px', // Adjust the height as needed
-        zIndex: 2,
-        marginTop: '50px', // Add this line to move the element lower
-        fontSize: '40px', // Adjust the font size as needed
-        color: 'black', // Adjust the text color as needed
-    },
-};
