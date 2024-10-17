85e0d77f1ef11cde9e01362754ea0655c98345d2
scanderson6 <scanderson6@crimson.ua.edu>
2024-10-17 13:09:27 -0500

    updating folder locations

---
+++ frontend/CS495-frontend/CS495-frontend/ClientApp/src/components/About-Us.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+import 'About-Us.css'
+
+export class About-Us extends Component {
+    static displayName = About - Us.name;
+
+    render() {
+        return (
+            <>
+                <p>This is the ABout Us page</p>
+            </>
+        );
+    }
+}
