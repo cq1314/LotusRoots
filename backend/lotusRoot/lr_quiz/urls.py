6bca8fbd670b068cb9141dc8449c46be270bc9dc
cq1314 <cindyqiu1314@gmail.com>
2024-10-17 13:01:43 -0500

    Added survey question API and connected MySQL to django

--- backend/lotusRoot/lr_quiz/urls.py
+++ backend/lotusRoot/lr_quiz/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import SurveyQuestionViewSet, SurveyResponseViewSet
 
+# Create a router for ViewSets
 router = DefaultRouter()
 router.register(r'questions', SurveyQuestionViewSet)
 router.register(r'responses', SurveyResponseViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    #path('api/', include(router.urls)),  # Include API URLs for your viewsets
+    path('', include(router.urls)), 
 ]
