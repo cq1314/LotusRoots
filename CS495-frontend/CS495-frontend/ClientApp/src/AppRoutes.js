8f84059a12280b67011139e7a31dd31f0b9f7554
scanderson6 <scanderson6@crimson.ua.edu>
2024-10-27 22:00:17 -0500

    Mostly completed About-Us Page

--- CS495-frontend/CS495-frontend/ClientApp/src/AppRoutes.js
+++ CS495-frontend/CS495-frontend/ClientApp/src/AppRoutes.js
@@ -1,6 +1,8 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { AboutUs } from './components/About-Us';
+
 
 const AppRoutes = [
   {
@@ -14,6 +16,10 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
+  },
+  {
+    path: '/about-us',
+    element: <AboutUs />
   }
 ];
 
--- CS495-frontend/CS495-frontend/ClientApp/src/components/About-Us.js
+++ CS495-frontend/CS495-frontend/ClientApp/src/components/About-Us.js
@@ -1,13 +1,71 @@
 ﻿import React, { Component } from 'react';
-import 'About-Us.css'
+import './About-Us.css';
 
-export class About-Us extends Component {
-    static displayName = About - Us.name;
+export class AboutUs extends Component {
+    static displayName = AboutUs.name;
 
     render() {
         return (
             <>
-                <p>This is the ABout Us page</p>
+                
+                <div className="pink-block">
+                </div>
+                <div className="pink-block2">
+                </div>
+                <div className="pink-block3">
+                </div>
+                <div className="firsttext">
+                    <h1></h1>
+                </div>
+                <div className="firstinformation">
+                    <p>Our mission is to consolidate resources and information regarding Japanese culture in the United States, starting  with the southeastern region our plan is to grow to encompass the entire United State. We aim to provide information on events, organizations, and culture in general.</p>
+                    <br></br>
+                    <br></br>
+                    <p>We strive not only to provide information but also to offer insight into the community. Our articles cover recent events and the Japanese culture community as a whole, and we provide reviews of local Japanese establishments and experiences.</p>
+                </div>
+                
+                <div className="secondtext">
+                    <h1></h1>
+                </div>
+                <div className="secondinformation">
+                    <p>We strive for the extraordinary! Our passion is to share the wonders of Japanese culture with people everywhere, inspiring them to embark on their own explorations and spread the beauty of these traditions far and wide.</p>
+                    <br></br>
+                    <p>Let us not be content with the ordinary, but seek out and create experiences that are truly amazing. Together, we can enrich our lives and the lives of others by embracing the richness of Japanese culture and all the wisdom and beauty it has to offer. So let us aim high and always remember that the greatest experiences are waiting for us to discover them!</p>
+                </div>
+
+            <div className="quarter-screen-container">
+                <div className="text-and-image">
+                    <div className="centered-text">
+                        <h1>OUR MISSION</h1>
+                    </div>
+                    <img src={require('../assets/pinkumbrella.jpeg')} className="centered-image" />
+                </div>
+            </div>
+
+            <div className="quarter-screen-container2">
+                <div className="text-and-image">
+                    <div className="centered-text">
+                        <h1>EXPERIENCE THE</h1>
+                        <br></br>
+                        <h1>EXTRAORDINARY</h1>
+                    </div>
+                    <img src={require('../assets/pinktree.jpeg')} className="centered-image" />
+                </div>
+            </div>
+
+            <div className="quarter-screen-container3">
+                <h1>CONTACT US</h1>
+            </div>
+
+            
+            <div className="contact-form">
+                <input type="text" placeholder="Name" className="short-input" />
+                <input type="email" placeholder="Email" className="short-input" />
+                <textarea placeholder="Your Question" className="long-input"></textarea>
+                <button className="question-button">Submit Question</button>
+            </div>
+
+
             </>
         );
     }
