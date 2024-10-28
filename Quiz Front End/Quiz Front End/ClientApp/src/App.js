342639d53303501ca750fb61025bc98e73cd6f6d
separal <121459329+separal@users.noreply.github.com>
2024-10-28 13:48:01 -0500

    Update App.js

--- Quiz Front End/Quiz Front End/ClientApp/src/App.js
+++ Quiz Front End/Quiz Front End/ClientApp/src/App.js
@@ -1,3 +1,27 @@
+/*import React, { useEffect, useState } from 'react';
+
+function SurveyQuestions() {
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/api/surveyquestions/')
+            .then(response => response.json())
+            .then(data => setQuestions(data));
+    }, []);
+
+    return (
+        <div>
+            <h1>Survey Questions</h1>
+            <ul>
+                {questions.map(question => (
+                    <li key={question.id}>{question.text}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default SurveyQuestions;*/
 import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
@@ -5,18 +29,18 @@ import { Layout } from './components/Layout';
 import './custom.css';
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  render() {
-    return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <Routes>
+                    {AppRoutes.map((route, index) => {
+                        const { element, ...rest } = route;
+                        return <Route key={index} {...rest} element={element} />;
+                    })}
+                </Routes>
+            </Layout>
+        );
+    }
 }
